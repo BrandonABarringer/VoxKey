a1ea6b9dc71c36c848bf267635d7c9db0feb3d91
Brandon Barringer <barringerbrandon@gmail.com>
2026-05-13 17:37:20 -0500

    Increase clipboard restore delay to 250ms

--- VoxKey/Utilities/Constants.swift
+++ VoxKey/Utilities/Constants.swift
@@ -5,7 +5,7 @@ enum Constants {
     static let rightOptionKeyCode: CGKeyCode = 61
     static let rightCtrlKeyCode: CGKeyCode = 62
     static let activationKeyCode: CGKeyCode = 62  // Right Control (change to 61 for Right Option)
-    static let clipboardRestoreDelay: TimeInterval = 0.1
+    static let clipboardRestoreDelay: TimeInterval = 0.25
     static let defaultModel = "base"
     static let whisperModels = ["tiny", "base", "small", "medium", "large-v3-turbo"]
     static let bundleIdentifier = "com.voxkey.VoxKey"
